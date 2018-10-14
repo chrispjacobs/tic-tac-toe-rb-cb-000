e9b6a0d4b9b848250b85be1bf9393f2c6c42a0e5
Christopher Jacobs <chrisj.3222@gmail.com>
2018-10-14 01:37:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,3 +70,57 @@ end
 
 board = ["X", " ", " ", "X", " ", "O", " ", " ", " "]
 puts turn_count(board)
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    position_1 = win_combo[0]
+    position_2 = win_combo[1]
+    position_3 = win_combo[2]
+    if board[position_1] == "X" && board[position_2] == "X" && board[position_3] == "X"
+      return [position_1, position_2, position_3]
+    elsif
+      board[position_1] == "O" && board[position_2] == "O" && board[position_3] == "O"
+      return [position_1, position_2, position_3]
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |space|
+  space != " "
+end
+end
+
+def draw?(board)
+  if full?(board) == true &&
+    won?(board) == false
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if full?(board) == true
+    return true
+  elsif won?(board) != false
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+unless won?(board) == false
+  pos_1 = won?(board)[0]
+  pos_2 = won?(board)[1]
+  pos_3 = won?(board)[2]
+  if board[pos_1] == "X" && board[pos_2] == "X" && board[pos_3] == "X"
+    return "X"
+  elsif board[pos_1] == "O" && board[pos_2] == "O" && board[pos_3] == "O"
+    return "O"
+  end
+end
+return nil
+end
