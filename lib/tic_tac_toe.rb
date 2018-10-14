b0db5bb526602523878aa8b678670b09e343725b
Christopher Jacobs <chrisj.3222@gmail.com>
2018-10-14 01:28:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,3 +31,11 @@ def position_taken?(board, index)
     return false
   end
 end
+
+def valid_move?(board, index)
+  if index >= 0 && index <= 8 && position_taken?(board, index) == false
+    return true
+  else
+    return false
+  end
+end
