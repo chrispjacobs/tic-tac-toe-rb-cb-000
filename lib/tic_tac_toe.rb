24ad3336bdc9ebf8e8120853eb5875b39d2c35df
Christopher Jacobs <chrisj.3222@gmail.com>
2018-10-14 01:27:26 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,25 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2], # top row
+  [3, 4, 5], # middle row
+  [6, 7, 8], # bottom row
+  [0, 3, 6], # left column
+  [1, 4, 7], # middle column
+  [2, 5, 8], # right column
+  [0, 4, 8], # first diagonal pattern
+  [2, 4, 6] # second diagonal pattern
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  return input.to_i - 1
+end
+def move(array, index, value = "X")
+  array[index] = value
+end
